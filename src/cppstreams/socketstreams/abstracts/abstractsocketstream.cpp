745fe2e8ea28287af9624ccfe5c901ec94dd8e24
Роман Кулага <romankulaha2010@gmail.com>
2023-03-20 21:21:53 +0200

    Correcting socket shutdown.

--- src/cppstreams/socketstreams/abstracts/abstractsocketstream.cpp
+++ src/cppstreams/socketstreams/abstracts/abstractsocketstream.cpp
@@ -6,4 +6,13 @@ AbstractSocketStream::AbstractSocketStream(
     boost::asio::ip::tcp::socket&& socket)
     : socket(std::move(socket)) {}
 
-AbstractSocketStream::~AbstractSocketStream() { socket.close(); }
+AbstractSocketStream::~AbstractSocketStream() {
+  if (socket.is_open()) {
+    try {
+      socket.shutdown(boost::asio::socket_base::shutdown_both);
+      socket.close();
+    } catch (...) {
+      // suppress
+    }
+  }
+}
